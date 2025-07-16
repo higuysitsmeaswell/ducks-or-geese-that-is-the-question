511fd76ead5c7129357f62530e570dbf63bf48db
higuysitsmeaswell <corymj2014@gmail.com>
2025-07-16 17:59:57 +0100

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -29,6 +29,8 @@ answer_box1.move_ip(20,270)
 answer_box2.move_ip(370,270)
 answer_box3.move_ip(20,450)
 answer_box4.move_ip(370,450)
+game_over = False
+game_complete = False
 
 def draw():
     screen.fill("indigo")
@@ -39,13 +41,62 @@ def draw():
     for i in answer_boxes:
         screen.draw.filled_rect(i,"violet")
     marquee_msg = "Welcome to the quiz master"
-    screen.draw.textbox(marquee_msg,marquee_box,color = "white")
-    screen.draw.textbox(question[0],question_box,color = "cyan")
+    screen.draw.textbox(marquee_msg,marquee_box,color = "white",shadow = (0.5,0.5),scolor = "grey")
+    screen.draw.textbox(question[0],question_box,color = "cyan",shadow = (0.5,0.5),scolor = "blue")
+    index = 1
+    for i in answer_boxes:
+        screen.draw.textbox(question[index],i,color = "red",shadow = (0.5,0.5),scolor = "maroon")
+        index += 1
+    screen.draw.textbox(str(timeleft),timer_box,color = "cyan",shadow = (0.5,0.5),scolor = "purple")
+    screen.draw.textbox("skip",skip_box,color = "blue",angle = -90,shadow = (0.5,0.5),scolor = "navy")
+
+def on_mouse_down(pos):
+    global answer_boxes
+    index = 1
+    for i in answer_boxes:
+        if i.collidepoint(pos):
+            if index is int(question[5]):
+
+                correctanswer()
+            else:
+                gameover()
+        index +=1
+    if skip_box.collidepoint(pos):
+        skip_question()
+
+def correctanswer():
+    global score, question, timeleft, game_complete
+    score +=1
+    if score == 11:
+        print("hello")
+        
+        gamecomplete()
+    if questions:
+
+        question = split_question()
+        timeleft = 10
+    else:
+        gameover()
+
+def gameover():
+    global question, timeleft, game_over
+    message = f"Game Over, you got: {score} questions correct."
+    question = [message,"-","-","-","-"]
+    timeleft = 0
+    game_over = True
+            
+def gamecomplete():
+    global question, timeleft, game_complete
+    message = "You win."
+    question = [message,"-","-","-","-"]
+    timeleft = 0
+    game_complete = True
 
 def move_marquee():
     marquee_box.x-=2
     if marquee_box.right<0:
         marquee_box.left = WIDTH
+    
 
 def update():
     move_marquee()
@@ -63,4 +114,20 @@ def split_question():
     return questions.pop(0).split(",")
 read_question()
 question = split_question()
+
+def update_timer():
+    global timeleft
+    if timeleft:
+        timeleft -=1
+    else:
+        gameover()
+clock.schedule_interval(update_timer,1)
+
+def skip_question():
+    global questions, game_over, question, timeleft
+    if questions and not game_over:
+        question = split_question()
+        timeleft = 10
+    else:
+        gameover()
 pgzrun.go()
